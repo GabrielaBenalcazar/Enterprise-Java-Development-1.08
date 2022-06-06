ed3654243513f6243bffa8464913af7053d1ba11
Gabriela Benalcazar <68346985+GabrielaBenalcazar@users.noreply.github.com>
2022-06-06 22:13:09 +0200

    se me habia olvidado hacer commit sorry

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.math.BigDecimal;
+import java.math.MathContext;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BigDecimal b1 = new BigDecimal("4.2585");
+        System.out.println(roundBigDec(b1));
+    }
+    public static double roundBigDec(BigDecimal bigDec){
+        MathContext m = new MathContext(3); // 3 precision
+        BigDecimal b2 = bigDec.round(m);
+        return b2.doubleValue();
     }
 }
